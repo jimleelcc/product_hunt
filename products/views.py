0a02bba8d2293daa2c01a096abb21694af9dc7f2
jimleelcc <jimleelcc@163.com>
2019-02-01 10:15:36 +0800

    初步完成了发布页面的制作

--- products/views.py
+++ products/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+# Create your views here.
 
 
 # Create your views here.
 def product_list(request):
     return render(request, 'product_list.html')
+
+
+def publish(request):
+    if request.method == 'GET':
+        return render(request, 'publish.html')
+    elif request.method == 'POST':
+        app_name = request.POST['APP名称']
+        intro = request.POST['介绍']
+        url = request.POST['APP链接']
+        icon = request.POST['APP图标']
+        image = request.POST['大图']
+        return render(request, 'publish.html')
